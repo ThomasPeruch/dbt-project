1b554851ffebe0323c5b6c13479b42d15e9a8e7b
Thomas Peruch <peruchthomas@gmail.com>
2025-11-07 16:01:45 -0300

    First Version of pacientes table created and deleting unnecessary files

---
+++ dbt_project/models/clean/paciente.sql
@@ -0,0 +1,31 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table', schema='clean') }}
+
+select 
+    id_paciente as id_origem,
+    INITCAP(TRIM( nome)) as nome,
+    REGEXP_REPLACE( cpf, '[^0-9]', '', 'g') as cpf,
+    data_nascimento as data_nascimento,
+    CASE
+        WHEN UPPER(sexo) = 'M' THEN 'Masculino'
+        WHEN UPPER(sexo) = 'MACULINO' THEN 'Masculino'
+        WHEN UPPER(sexo) = 'HOMEM' THEN 'Masculino'
+        WHEN UPPER(sexo) = 'MULHER' THEN 'Feminino'
+        WHEN UPPER(sexo) = 'F' THEN 'Feminino'
+        WHEN UPPER(sexo) = 'FEMININO' THEN 'Feminino'
+    END AS sexo,
+    INITCAP(TRIM( nome_mae)) as nome_mae,
+    LOWER( email) as email,
+    telefone as telefone
+from {{ source('raw', 'paciente') }}
+where data_nascimento is not null
+    and (cpf is not null and TRIM(cpf) != '')
+
